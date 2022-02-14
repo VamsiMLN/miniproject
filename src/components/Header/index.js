fe088fb87825982774e78f13c9f49f002d85b13d
vamsi <narasimhavamsi1994@gmail.com>
2022-02-14 02:59:15 +0000

    updated

---
+++ src/components/Header/index.js
@@ -0,0 +1,21 @@
+import './index.css'
+
+const Header = () => (
+  <>
+    <div className="header-container">
+      <div className="image-container">
+        <img
+          className="header-logo"
+          alt=""
+          src="https://res.cloudinary.com/djdh5bkl5/image/upload/v1644799340/Group_7732_ixqanv.png"
+        />
+        <img
+          className="menu-logo"
+          alt=""
+          src="https://res.cloudinary.com/djdh5bkl5/image/upload/v1644799435/menu_owku8j.png"
+        />
+      </div>
+    </div>
+  </>
+)
+export default Header
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,6 +1,66 @@
-const Home = () => (
-  <div>
-    <h1>vamsi</h1>
-  </div>
-)
+import {Component} from 'react'
+import Cookies from 'js-cookie'
+import Header from '../Header'
+import TopratedBooks from '../TopratedBooks'
+import './index.css'
+
+class Home extends Component {
+  state = {booksData: []}
+
+  componentDidMount() {
+    this.fetchBookDetails()
+  }
+
+  fetchBookDetails = async () => {
+    const jwtToken = Cookies.get('jwt_token')
+    const apiUrl = 'https://apis.ccbp.in/book-hub/top-rated-books'
+    const options = {
+      method: 'GET',
+      headers: {
+        Authorization: `Bearer ${jwtToken}`,
+      },
+    }
+    const response = await fetch(apiUrl, options)
+    const data = await response.json()
+    console.log(data)
+    const updatedData = data.books.map(eachBook => ({
+      authorName: eachBook.author_name,
+      coverPic: eachBook.cover_pic,
+      id: eachBook.id,
+      title: eachBook.title,
+    }))
+    this.setState({booksData: updatedData})
+  }
+
+  render() {
+    const {booksData} = this.state
+    console.log(booksData)
+    return (
+      <>
+        <Header />
+        <div className="home-main-container">
+          <div className="home-container">
+            <h1 className="home-heading">Find Your Next Favorite Book</h1>
+            <p className="home-pera">
+              You are in the right place.Tell us what titles or genres you have
+              enjoyed in the past,and we will give you suprisingly insightful
+              recommendations.
+            </p>
+            <button type="button" className="button">
+              Find Books
+            </button>
+          </div>
+          <div>
+            <ul className="un-order">
+              {booksData.map(eachItem => (
+                <TopratedBooks details={eachItem} />
+              ))}
+            </ul>
+          </div>
+        </div>
+      </>
+    )
+  }
+}
+
 export default Home
